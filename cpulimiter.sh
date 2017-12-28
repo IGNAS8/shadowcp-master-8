00d61600e8b795b4807c59cbff506e97d375287f
IGNAS8 <34920434+IGNAS8@users.noreply.github.com>
2017-12-28 19:32:32 +0200

    Initial commit

---
+++ cpulimiter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cpu=`cat cpuname.txt`
+while [ 1=1 ]
+do
+	wait=$(shuf -i2-6 -n1)
+	load=$(shuf -i490-576 -n1)
+	cpulimit -e ${cpu} -l ${load} -b -q
+	sleep ${wait}
+	pkill cpulimit
+done
---
+++ winbuild-allarch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+make distclean || echo clean
+rm -f config.status
+./autogen.sh || echo done
+CFLAGS="-O3 -march=core-avx2 -Wall" CXXFLAGS="$CFLAGS -std=gnu++11 -fpermissive" ./configure --with-curl
+make -j 4
+strip -s shadowcpu.exe
+mv shadowcpu.exe shadowcpu-aes-avx2.exe
+
+make clean || echo clean
+rm -f config.status
+./autogen.sh || echo done
+CFLAGS="-O3 -march=corei7-avx -Wall" CXXFLAGS="$CFLAGS -std=gnu++11 -fpermissive" ./configure --with-curl
+make -j 4
+strip -s shadowcpu.exe
+mv shadowcpu.exe shadowcpu-aes-avx.exe
+
+make clean || echo clean
+rm -f config.status
+./autogen.sh || echo done
+CFLAGS="-O3 -maes -msse4.2 -Wall" CXXFLAGS="$CFLAGS -std=gnu++11 -fpermissive" ./configure --with-curl
+make -j 4
+strip -s shadowcpu.exe
+mv shadowcpu.exe shadowcpu-aes-sse42.exe
+
+make clean || echo clean
+rm -f config.status
+./autogen.sh || echo done
+CFLAGS="-O3 -march=corei7 -Wall" CXXFLAGS="$CFLAGS -std=gnu++11 -fpermissive" ./configure --with-curl
+make -j 4
+strip -s shadowcpu.exe
+mv shadowcpu.exe shadowcpu-sse42.exe
+
+make clean || echo clean
+rm -f config.status
+./autogen.sh || echo done
+CFLAGS="-O3 -march=core2 -Wall" CXXFLAGS="$CFLAGS -std=gnu++11 -fpermissive" ./configure --with-curl
+make -j 4
+strip -s shadowcpu.exe
+mv shadowcpu.exe shadowcpu-sse2.exe
+
+make clean || echo done
+
+
